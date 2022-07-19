3bfef3613c533c3183cabf8f46306da5b727e591
minmin.liu <minmin.liu@linkedcare.cn>
2022-07-19 18:25:43 +0800

    feat:3.3指定写入的列

---
+++ src/main/java/net/educoder/pojo/WriteOrderData.java
@@ -0,0 +1,33 @@
+package net.educoder.pojo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * 基础数据类
+ *
+ * @author Jiaju Zhuang
+ **/
+@Getter
+@Setter
+@EqualsAndHashCode
+public class WriteOrderData {
+    @ExcelProperty(value = "字符串标题", index = 0)
+    private String string;
+    @ExcelProperty(value = "日期标题", index = 1)
+    private Date date;
+    ///**
+    // * 这里设置3 会导致第二列空的
+    // */
+    //@ExcelProperty(value = "数字标题", index = 3)
+    //private Double doubleData;
+
+    // 这里需要注意 在使用ExcelProperty注解的使用，如果想不空列则需要加入order字段，
+    // 而不是index,order会忽略空列，然后继续往后，而index，不会忽略空列，在第几列就是第几列。
+    @ExcelProperty(value = "整数标题", order = 5)
+    private Integer intData;
+}
--- src/test/java/net/educoder/write/WriteTest.java
+++ src/test/java/net/educoder/write/WriteTest.java
@@ -2,13 +2,18 @@ package net.educoder.write;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.google.common.collect.Lists;
 import net.educoder.pojo.DemoData;
+import net.educoder.pojo.WriteIndexData;
+import net.educoder.pojo.WriteOrderData;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author minmin.liu
@@ -27,19 +32,71 @@ public class WriteTest {
     @Test
     public void simpleWrite() {
         //写法1
-        String fileName = path + "simpleWrite01" + ".xlsx";
+        String fileName = path + "simpleWrite01.xlsx";
         EasyExcel.write(fileName, DemoData.class)
                 .sheet("简单写")
                 .doWrite(this::data);
         //写法2
-        fileName = path + "simpleWrite02" + ".xlsx";
+        fileName = path + "simpleWrite02.xlsx";
         EasyExcel.write(fileName, DemoData.class).sheet("写法2").doWrite(data());
         //写法3
-        fileName = path + "simpleWrite03" + ".xlsx";
+        fileName = path + "simpleWrite03.xlsx";
         try (ExcelWriter excelWriter = EasyExcel.write(fileName, DemoData.class).build()) {
             WriteSheet writeSheet = EasyExcel.writerSheet("写法3").build();
             excelWriter.write(data(), writeSheet);
         }
+    }
+
+    /**
+     * 3.2:根据参数只导出指定列
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 根据自己或者排除自己需要的列
+     * <p>
+     * 3. 直接写即可
+     *
+     * @since 2.1.1
+     */
+    @Test
+    public void excludeOrIncludeWrite() {
+        //忽略某个字段
+        String fileName = path + "excludeWrite.xlsx";
+        // 根据用户传入字段 假设我们要忽略 date
+        Set<String> excludeColumnFieldNames = new HashSet<>();
+        excludeColumnFieldNames.add("date");
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为"忽略字段" 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoData.class).excludeColumnFieldNames(excludeColumnFieldNames).sheet("忽略字段")
+                .doWrite(data());
+
+        //只写入某个字段
+        fileName = path + "includeWrite.xlsx";
+        Set<String> includeColumnFieldNames = new HashSet<>();
+        includeColumnFieldNames.add("string");
+        EasyExcel.write(fileName, DemoData.class)
+                .includeColumnFieldNames(includeColumnFieldNames)
+                .sheet("只写入某些字段")
+                .doWrite(data());
+    }
+
+    /**
+     * 3.3 指定写入的列
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link WriteIndexData}
+     * <p>
+     * 2. 使用{@link ExcelProperty}注解指定写入的列
+     * <p>
+     * 3. 直接写即可
+     */
+    @Test
+    public void indexWrite() {
+        String fileName = path + "indexWrite.xlsx";
+        //会有空列
+        EasyExcel.write(fileName, WriteIndexData.class).sheet("指定写入的列").doWrite(writeIndexData());
+        //不会有空列
+        fileName = path + "orderWrite.xlsx";
+        EasyExcel.write(fileName, WriteOrderData.class).sheet("指定写入的列").doWrite(writeOrderData());
+
     }
 
     private List<DemoData> data() {
@@ -49,6 +106,32 @@ public class WriteTest {
             data.setString("数据" + i);
             data.setDate(new Date());
             data.setDoubleData(i * Math.random());
+            datas.add(data);
+        }
+        return datas;
+    }
+
+    private List<WriteIndexData> writeIndexData() {
+        List<WriteIndexData> datas = Lists.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            WriteIndexData data = new WriteIndexData();
+            data.setString("数据" + i);
+            data.setDate(new Date());
+            data.setDoubleData(i * Math.random());
+            //data.setIntData(i);
+            datas.add(data);
+        }
+        return datas;
+    }
+
+    private List<WriteOrderData> writeOrderData() {
+        List<WriteOrderData> datas = Lists.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            WriteOrderData data = new WriteOrderData();
+            data.setString("数据" + i);
+            data.setDate(new Date());
+            //data.setDoubleData(i * Math.random());
+            data.setIntData(i);
             datas.add(data);
         }
         return datas;
