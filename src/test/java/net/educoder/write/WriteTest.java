cbaebffbb02d383d935679093473ce87f94c372c
playboy <15060335532@139.com>
2022-07-24 18:44:41 +0800

    feat:3.9 根据模板写入

--- src/test/java/net/educoder/write/WriteTest.java
+++ src/test/java/net/educoder/write/WriteTest.java
@@ -379,6 +379,26 @@ public class WriteTest {
 
   }
 
+  /**
+   * 3.9 根据模板写入
+   * <p>
+   * 1. 创建excel对应的实体对象
+   * <p>
+   * 2. 使用{@link ExcelProperty}注解指定写入的列
+   * <p>
+   * 3. 使用withTemplate 写取模板
+   * <p>
+   * 4. 直接写即可
+   */
+  @Test
+  public void templateWrite() {
+    String templateFileName = path + "demo" + File.separator + "demo.xlsx";
+    String fileName = path + "templateWrite.xlsx";
+    // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+    EasyExcel.write(fileName, DemoData.class).withTemplate(templateFileName).sheet()
+        .doWrite(data());
+  }
+
   private List<DemoData> data() {
     List<DemoData> datas = Lists.newArrayList();
     for (int i = 0; i < 10; i++) {
